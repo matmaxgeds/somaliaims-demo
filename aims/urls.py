381720f6bd85f32acb5e05ac56075039ca5edec2
Arlus <arlusishmael@gmail.com>
2015-04-20 16:25:29 +0300

    Added management forms, views and CRUD page

--- aims/urls.py
+++ aims/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),
-    url(r'^', include('management.urls')),
+    url(r'^management/', include('management.urls')),
     url(r'^', include('data_entry.urls')),
     url(r'^', include('home.urls')),
     #url(r'^$', 'aims.views.home', name='home'),
