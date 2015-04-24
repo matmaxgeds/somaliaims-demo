d228fbc50d1e5c7c062e552195c9aca52e73aec2
Kenneth Kinyanjui <kenmbugua@gmail.com>
2015-04-24 17:27:46 +0300

    Refactoring the word config to conf

--- static/grappelli/tinymce/jscripts/tiny_mce/plugins/autosave/editor_plugin_src.js
+++ static/grappelli/tinymce/jscripts/tiny_mce/plugins/autosave/editor_plugin_src.js
@@ -85,7 +85,7 @@
 				return (time[2] ? multipels[time[2]] : 1) * parseInt(time);
 			};
 
-			// Default config
+			// Default conf
 			tinymce.each({
 				ask_before_unload : TRUE,
 				interval : '30s',
