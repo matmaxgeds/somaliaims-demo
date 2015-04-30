556e5e0e4291689559ee5583c4b483ff7966076d
Arlus <arlusishmael@gmail.com>
2015-04-30 16:52:38 +0300

    Added UI to management pages

--- management/forms.py
+++ management/forms.py
@@ -5,21 +5,39 @@ from django.forms.models import BaseModelFormSet
 
 
 class LocationForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(LocationForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Location
         exclude = ('id',)
 
 
 class SubLocationForm(forms.ModelForm):
+    #def __init__(self, *args, **kwargs):
+    #    super(SubLocationForm, self).__init__(*args, **kwargs)
+    #    self.fields['name'].widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = SubLocation
         exclude = ('id',)
 
 
 class OrganizationForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(OrganizationForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Organization
         fields = ('name',)
+
+
+class BaseSubLocationFormset(BaseModelFormSet):
+    def __init__(self, *args, **kwargs):
+        super(BaseSubLocationFormset, self).__init__(*args, **kwargs)
+        self.queryset = SubLocation.objects.none()
 
 
 class BaseOrganizationFormSet(BaseModelFormSet):
@@ -29,6 +47,10 @@ class BaseOrganizationFormSet(BaseModelFormSet):
 
 
 class ExchangeRateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ExchangeRateForm, self).__init__(*args, **kwargs)
+        self.fields['rateToSOM'].widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = ExchangeRate
         exclude = ('dateSet',)
@@ -41,8 +63,15 @@ class BaseSectorFormSet(BaseModelFormSet):
 
 
 class SectorForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(SectorForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+        self.fields['description'].widget.attrs['class'] = 'form-control'
+        self.fields['description'].widget.attrs['rows'] = '3'
+
     class Meta:
         model = Sector
         fields = ('name', 'description')
 
-SubLocationFormset = inlineformset_factory(Location, SubLocation, fields=('name',), can_delete=True, extra=1)
+SubLocationFormset = inlineformset_factory(Location, SubLocation, fields=('name',), can_delete=True, extra=1,
+                                           formset=BaseSubLocationFormset, form=SubLocationForm)
