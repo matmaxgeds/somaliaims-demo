108870c8cebd14bbc7b31e5c38a76411232de959
Kevin Ndung'u <kevgathuku@gmail.com>
2015-04-30 16:09:36 +0300

    Add Help Page view and basic template
    - Add help page view
    - Add matching URL
    - Add link to Help page in the base template
    - Add placeholder text to the Help Page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.views.generic.base import TemplateView
 
 
 def home(request):
     """Home page"""
     return render_to_response("home/homepage.html", locals(), context_instance=RequestContext(request))
+
+
+class HelpPageView(TemplateView):
+    """Help Page"""
+    template_name = "home/help.html"
