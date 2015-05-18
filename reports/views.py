6ec6cb4e6bde1fd287fab9b1f82a7a3e44f5258d
Arlus <arlusishmael@gmail.com>
2015-05-18 12:00:05 +0300

    Refactoring exporter views.

--- reports/views.py
+++ reports/views.py
@@ -7,9 +7,7 @@ from django.template.loader import get_template
 from django.http import HttpResponse
 
 
-def xls_gen(request):
-    import xlwt
-    from django.utils.six import moves
+def list_generator(request):
     allocated_projects = LocationAllocation.objects.values('project').distinct()
     pList = allocated_projects
     import sys
@@ -75,6 +73,13 @@ def xls_gen(request):
             x.update(y)
         pList = a
     filtered = ProjectFilter(request.GET, queryset=Project.objects.filter(id__in=pList))
+    return filtered
+
+
+def xls_gen(request):
+    import xlwt
+    from django.utils.six import moves
+    filtered = list_generator(request)
     response = HttpResponse(content_type='application/ms-excel')
     response['Content-Disposition'] = 'attachment; filename=projectlist.xls'
     wb = xlwt.Workbook(encoding='utf-8')
@@ -116,71 +121,7 @@ def xls_gen(request):
 
 def csv_gen(request):
     import csv
-    allocated_projects = LocationAllocation.objects.values('project').distinct()
-    pList = allocated_projects
-    import sys
-    if request.GET.getlist('locations') == [] and request.GET.getlist('sectors') == [] and request.GET.getlist(
-            'sublocations') == []:
-        sys.stderr.write("1 called")
-        allocated_projects = LocationAllocation.objects.values('project').distinct()
-        pList = allocated_projects
-    if request.GET.getlist('locations') != [] and request.GET.getlist('sectors') == [] and request.GET.getlist(
-            'sublocations') == []:
-        allocated_projects = LocationAllocation.objects.filter(
-            location__in=request.GET.getlist('locations')).distinct().values('project')
-        pList = allocated_projects
-    if request.GET.getlist('sectors') != [] and request.GET.getlist('locations') == [] and request.GET.get(
-            'sublocations') == []:
-        allocated_sectors = SectorAllocation.objects.filter(
-            sector__in=request.GET.getlist('sectors')).distinct().values('project')
-        pList = allocated_sectors
-    if request.GET.getlist('sublocations') != [] and request.GET.get('locations') == [] and request.GET.get(
-            'sectors') == []:
-        sublocations = LocationAllocation.objects.filter(
-            sublocations__in=request.GET.getlist('sublocations')).distinct().values('project')
-        pList = sublocations
-    if request.GET.getlist('sectors') != [] and request.GET.getlist('locations') != [] and request.GET.getlist(
-            'sublocations') == []:
-        a = LocationAllocation.objects.filter(
-            location__in=request.GET.getlist('locations')).distinct().values('project')
-        b = SectorAllocation.objects.filter(sector__in=request.GET.getlist('sectors')).distinct().values(
-            'project')
-        for x, y in zip(a, b):
-            x.update(y)
-        pList = a
-    if request.GET.getlist('sectors') != [] and request.GET.getlist('locations') != [] and request.GET.getlist(
-            'sublocations') != []:
-        a = LocationAllocation.objects.filter(
-            location__in=request.GET.getlist('locations')).distinct().values('project')
-        b = SectorAllocation.objects.filter(sector__in=request.GET.getlist('sectors')).distinct().values(
-            'project')
-        c = LocationAllocation.objects.filter(
-            sublocations__in=request.GET.getlist('sublocations')).distinct().values('project')
-        for x, y in zip(a, b):
-            x.update(y)
-        for x, y in zip(a, c):
-            x.update(y)
-        pList = a
-    if request.GET.getlist('sectors') != [] and request.GET.getlist('sublocations') != [] and request.GET.getlist(
-            'locations') == []:
-        b = SectorAllocation.objects.filter(sector__in=request.GET.getlist('sectors')).distinct().values(
-            'project')
-        c = LocationAllocation.objects.filter(
-            sublocations__in=request.GET.getlist('sublocations')).distinct().values('project')
-        for x, y in zip(b, c):
-            x.update(y)
-        pList = b
-
-    if request.GET.getlist('locations') != [] and request.GET.getlist('sublocations') != [] and request.GET.getlist(
-            'sectors') == []:
-        a = LocationAllocation.objects.filter(
-            location__in=request.GET.getlist('locations')).distinct().values('project')
-        c = LocationAllocation.objects.filter(
-            sublocations__in=request.GET.getlist('sublocations')).distinct().values('project')
-        for x, y in zip(a, c):
-            x.update(y)
-        pList = a
-    filtered = ProjectFilter(request.GET, queryset=Project.objects.filter(id__in=pList))
+    filtered = list_generator(request)
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="projectlist.csv"'
     writer = csv.writer(response)
@@ -195,71 +136,7 @@ def csv_gen(request):
 
 def pdf_gen(request):
     import weasyprint
-    allocated_projects = LocationAllocation.objects.values('project').distinct()
-    pList = allocated_projects
-    import sys
-    if request.GET.getlist('locations') == [] and request.GET.getlist('sectors') == [] and request.GET.getlist(
-            'sublocations') == []:
-        sys.stderr.write("1 called")
-        allocated_projects = LocationAllocation.objects.values('project').distinct()
-        pList = allocated_projects
-    if request.GET.getlist('locations') != [] and request.GET.getlist('sectors') == [] and request.GET.getlist(
-            'sublocations') == []:
-        allocated_projects = LocationAllocation.objects.filter(
-            location__in=request.GET.getlist('locations')).distinct().values('project')
-        pList = allocated_projects
-    if request.GET.getlist('sectors') != [] and request.GET.getlist('locations') == [] and request.GET.get(
-            'sublocations') == []:
-        allocated_sectors = SectorAllocation.objects.filter(
-            sector__in=request.GET.getlist('sectors')).distinct().values('project')
-        pList = allocated_sectors
-    if request.GET.getlist('sublocations') != [] and request.GET.get('locations') == [] and request.GET.get(
-            'sectors') == []:
-        sublocations = LocationAllocation.objects.filter(
-            sublocations__in=request.GET.getlist('sublocations')).distinct().values('project')
-        pList = sublocations
-    if request.GET.getlist('sectors') != [] and request.GET.getlist('locations') != [] and request.GET.getlist(
-            'sublocations') == []:
-        a = LocationAllocation.objects.filter(
-            location__in=request.GET.getlist('locations')).distinct().values('project')
-        b = SectorAllocation.objects.filter(sector__in=request.GET.getlist('sectors')).distinct().values(
-            'project')
-        for x, y in zip(a, b):
-            x.update(y)
-        pList = a
-    if request.GET.getlist('sectors') != [] and request.GET.getlist('locations') != [] and request.GET.getlist(
-            'sublocations') != []:
-        a = LocationAllocation.objects.filter(
-            location__in=request.GET.getlist('locations')).distinct().values('project')
-        b = SectorAllocation.objects.filter(sector__in=request.GET.getlist('sectors')).distinct().values(
-            'project')
-        c = LocationAllocation.objects.filter(
-            sublocations__in=request.GET.getlist('sublocations')).distinct().values('project')
-        for x, y in zip(a, b):
-            x.update(y)
-        for x, y in zip(a, c):
-            x.update(y)
-        pList = a
-    if request.GET.getlist('sectors') != [] and request.GET.getlist('sublocations') != [] and request.GET.getlist(
-            'locations') == []:
-        b = SectorAllocation.objects.filter(sector__in=request.GET.getlist('sectors')).distinct().values(
-            'project')
-        c = LocationAllocation.objects.filter(
-            sublocations__in=request.GET.getlist('sublocations')).distinct().values('project')
-        for x, y in zip(b, c):
-            x.update(y)
-        pList = b
-
-    if request.GET.getlist('locations') != [] and request.GET.getlist('sublocations') != [] and request.GET.getlist(
-            'sectors') == []:
-        a = LocationAllocation.objects.filter(
-            location__in=request.GET.getlist('locations')).distinct().values('project')
-        c = LocationAllocation.objects.filter(
-            sublocations__in=request.GET.getlist('sublocations')).distinct().values('project')
-        for x, y in zip(a, c):
-            x.update(y)
-        pList = a
-    filtered = ProjectFilter(request.GET, queryset=Project.objects.filter(id__in=pList))
+    filtered = list_generator(request)
     context = {'filtered': filtered}
     template = get_template('reports/report.html')
     html = template.render(RequestContext(request, context))
@@ -272,8 +149,6 @@ def project_list(request):
     loc_form = LocationForm()
     sec_form = SectorForm()
     sub_form = SublocationForm()
-    allocated_projects = LocationAllocation.objects.values('project').distinct()
-    pList = allocated_projects
     url = request.GET.urlencode()
     pdf_url = "http://127.0.0.1:8000/reports/export-pdf/?" + url
     csv_url = "http://127.0.0.1:8000/reports/export-csv/?" + url
@@ -282,63 +157,5 @@ def project_list(request):
     exporters = False
     if "reports" in page:
         exporters = True
-
-    if request.GET.getlist('locations') != [] and request.GET.getlist('sectors') == [] and request.GET.getlist(
-            'sublocations') == []:
-        allocated_projects = LocationAllocation.objects.filter(
-            location__in=request.GET.getlist('locations')).distinct().values('project')
-        pList = allocated_projects
-    if request.GET.getlist('sectors') != [] and request.GET.getlist('locations') == [] and request.GET.getlist(
-            'sublocations') == []:
-        allocated_sectors = SectorAllocation.objects.filter(
-            sector__in=request.GET.getlist('sectors')).distinct().values('project')
-        pList = allocated_sectors
-    if request.GET.getlist('sublocations') != [] and request.GET.getlist('locations') == [] and request.GET.getlist(
-            'sectors') == []:
-        sublocations = LocationAllocation.objects.filter(
-            sublocations__in=request.GET.getlist('sublocations')).distinct().values('project')
-        pList = sublocations
-    if request.GET.getlist('sectors') != [] and request.GET.getlist('locations') != [] and request.GET.getlist(
-            'sublocations') == []:
-        a = LocationAllocation.objects.filter(
-            location__in=request.GET.getlist('locations')).distinct().values('project')
-        b = SectorAllocation.objects.filter(sector__in=request.GET.getlist('sectors')).distinct().values(
-            'project')
-        for x, y in zip(a, b):
-            x.update(y)
-        pList = a
-    if request.GET.getlist('sectors') != [] and request.GET.getlist('locations') != [] and request.GET.getlist(
-            'sublocations') != []:
-        a = LocationAllocation.objects.filter(
-            location__in=request.GET.getlist('locations')).distinct().values('project')
-        b = SectorAllocation.objects.filter(sector__in=request.GET.getlist('sectors')).distinct().values(
-            'project')
-        c = LocationAllocation.objects.filter(
-            sublocations__in=request.GET.getlist('sublocations')).distinct().values('project')
-        for x, y in zip(a, b):
-            x.update(y)
-        for x, y in zip(a, c):
-            x.update(y)
-        pList = a
-    if request.GET.getlist('sectors') != [] and request.GET.getlist('sublocations') != [] and request.GET.getlist(
-            'locations') == []:
-        b = SectorAllocation.objects.filter(sector__in=request.GET.getlist('sectors')).distinct().values(
-            'project')
-        c = LocationAllocation.objects.filter(
-            sublocations__in=request.GET.getlist('sublocations')).distinct().values('project')
-        for x, y in zip(b, c):
-            x.update(y)
-        pList = b
-
-    if request.GET.getlist('locations') != [] and request.GET.getlist('sublocations') != [] and request.GET.getlist(
-            'sectors') == []:
-        a = LocationAllocation.objects.filter(
-            location__in=request.GET.getlist('locations')).distinct().values('project')
-        c = LocationAllocation.objects.filter(
-            sublocations__in=request.GET.getlist('sublocations')).distinct().values('project')
-        for x, y in zip(a, c):
-            x.update(y)
-        pList = a
-
-    filter = ProjectFilter(request.GET, queryset=Project.objects.filter(id__in=pList))
+    filtered = list_generator(request)
     return render_to_response('reports/index.html', locals(), context_instance=RequestContext(request))
