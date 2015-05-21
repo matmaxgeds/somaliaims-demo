0548424b96b96b713648dec00b7ba85d533efba8
Arlus <arlusishmael@gmail.com>
2015-05-21 23:07:15 +0300

    Allowing all users to view project details page, reducing report page font

--- data_entry/urls.py
+++ data_entry/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^$', login_required(ProjectListView.as_view()), name='dashboard'),
     url(r'^project/create/$', login_required(ProjectCreateView.as_view()), name='project_add'),
     url(r'^project/update/(?P<pk>[^/]+)/$', login_required(ProjectUpdateView.as_view()), name='project_update'),
-    url(r'^project/details/(?P<pk>[^/]+)/$', login_required(ProjectDetailView.as_view()), name='project_details'),
+    url(r'^project/details/(?P<pk>[^/]+)/$', ProjectDetailView.as_view(), name='project_details'),
     url(r'^download/(?P<pk>[^/]+)/$', download_handler, name='download'),
     url(r'^project/delete/(?P<pk>[^/]+)/$', login_required(ProjectDelete.as_view()),
         name='project_delete'),
