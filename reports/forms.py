f583e2bc1acdac1f8de7b13dfd0f795a0c623b59
Arlus <arlusishmael@gmail.com>
2015-06-01 08:13:05 +0300

    Solving bug that prevented new location and sectors from appering in drop down lists

--- reports/forms.py
+++ reports/forms.py
@@ -1,33 +1,49 @@
 from django import forms
 from management.models import Location, Sector, SubLocation
-from data_entry.models import LocationAllocation
+#from data_entry.models import LocationAllocation
 
-LOCATIONS = [[x.id, x.name] for x in Location.objects.all()]
 
-SECTORS = [[x.id, x.name] for x in Sector.objects.all()]
+def get_locations():
+    return [[x.id, x.name] for x in Location.objects.all()]
+
+
+def get_sectors():
+    return [[x.id, x.name] for x in Sector.objects.all()]
 
 # SUBLOCATIONS = [[x.id, x.name] for x in LocationAllocation.sublocations.all()]
 
-SUBLOCATIONS = [[x.id, x.name] for x in SubLocation.objects.all()]
+
+def get_sublocations():
+    return [[x.id, x.name] for x in SubLocation.objects.all()]
 
 
 class LocationForm(forms.Form):
-    locations = forms.MultipleChoiceField(choices=LOCATIONS, label="")
+    def __init__(self, *args, **kwargs):
+        super(LocationForm, self).__init__(*args, **kwargs)
+        self.fields['locations'] = forms.MultipleChoiceField(choices=get_locations(), label="")
 
 
 class SectorForm(forms.Form):
-    sectors = forms.MultipleChoiceField(choices=SECTORS, label="")
+    def __init__(self, *args, **kwargs):
+        super(SectorForm, self).__init__(*args, **kwargs)
+        self.fields['sectors'] = forms.MultipleChoiceField(choices=get_sectors(), label="")
 
 
 class SublocationForm(forms.Form):
-    sublocations = forms.MultipleChoiceField(choices=SUBLOCATIONS, label="")
+    def __init__(self, *args, **kwargs):
+        super(SublocationForm, self).__init__(*args, **kwargs)
+        self.fields['sublocations'] = forms.MultipleChoiceField(choices=get_sublocations(), label="")
 
 
 class SectorReportForm(forms.Form):
-    sector = forms.ChoiceField(choices=[('', '----------'), ] + SECTORS, label="Sector",
-                               widget=forms.Select(attrs={'onChange': "this.form.submit();"}))
+    def __init__(self, *args, **kwargs):
+        super(SectorReportForm, self).__init__(*args, **kwargs)
+        self.fields['sector'] = forms.ChoiceField(choices=[('', '----------'), ] + get_sectors(), label='Sector',
+                                                  widget=forms.Select(attrs={'onChange': "this.form.submit();"}))
 
 
 class LocationReportForm(forms.Form):
-    location = forms.ChoiceField(choices=[('', '----------'), ] + LOCATIONS, label="Location",
-                               widget=forms.Select(attrs={'onChange': "this.form.submit();"}))
+    def __init__(self, *args, **kwargs):
+        super(LocationReportForm, self).__init__(*args, **kwargs)
+        self.fields['location'] = forms.ChoiceField(choices=[('', '----------'), ] + get_locations(), label="Location",
+                                 widget=forms.Select(attrs={'onChange': "this.form.submit();"}))
